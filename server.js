3ae19bc8c91dc481e4d568cd7108021489102f65
wai wai <waiwaiphyochun@gmail.com>
2025-04-03 22:18:00 +0800

    404 error fix

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const logger = require("morgan");
@@ -13,7 +12,13 @@ const authRouter = require("./routes/auth");
 const usersRouter = require("./routes/users");
 const invoiceRoutes = require("./routes/invoiceRoutes"); 
 
-// Connect to MongoDB with error handling
+const app = express();
+
+// Middleware
+app.use(cors()); 
+app.use(express.json()); 
+app.use(logger("dev")); 
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log(`Connected to MongoDB ${mongoose.connection.name}`))
   .catch(err => {
@@ -21,10 +26,10 @@ mongoose.connect(process.env.MONGODB_URI)
     process.exit(1); 
   });
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-app.use(logger("dev"));
+// Root route
+app.get("/", (req, res) => {
+  res.send("QUO E-Invoice API is running!");
+});
 
 // Routes
 app.use("/test-jwt", testJwtRouter);
